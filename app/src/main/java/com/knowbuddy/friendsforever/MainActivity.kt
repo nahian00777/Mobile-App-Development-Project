6ebc805ff16f2ac616bf5d9b60ef45d64a4c4d63
Nahian Chowdhury <nahianchowdhury00777@gmail.com>
2024-12-24 18:40:31 +0600

    Initial Commit

---
+++ app/src/main/java/com/knowbuddy/friendsforever/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.knowbuddy.friendsforever
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.opening_page)
+    }
+}
---
+++ app/src/main/java/com/knowbuddy/friendsforever/frontend/pages/HomePage.kt
@@ -0,0 +1,16 @@
+package com.knowbuddy.friendsforever.frontend.pages
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.knowbuddy.friendsforever.R
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_page)
+    }
+}
