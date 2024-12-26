f32c9b8e5aba5a2c79c3d2f8e37ab933b8da5350
Nahian Chowdhury <nahianchowdhury00777@gmail.com>
2024-12-27 00:07:25 +0600

    HomePage

--- app/src/main/java/com/knowbuddy/friendsforever/MainActivity.kt
+++ app/src/main/java/com/knowbuddy/friendsforever/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.knowbuddy.friendsforever
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import com.knowbuddy.friendsforever.frontend.pages.HomePage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.opening_page)
+        val loginButton: Button = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, HomePage::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/knowbuddy/friendsforever/frontend/pages/HomePage.kt
+++ app/src/main/java/com/knowbuddy/friendsforever/frontend/pages/HomePage.kt
@@ -1,16 +1,12 @@
 package com.knowbuddy.friendsforever.frontend.pages
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.knowbuddy.friendsforever.R
 
-class HomePage : AppCompatActivity() {
+class HomePage : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
     }
 }
