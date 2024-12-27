178aef67f845e3114671d0fb8961c907b0f1a6cd
Nahian Chowdhury <nahianchowdhury00777@gmail.com>
2024-12-27 11:28:39 +0600

    Home Page Finishing

--- app/src/main/java/com/knowbuddy/friendsforever/frontend/pages/HomePage.kt
+++ app/src/main/java/com/knowbuddy/friendsforever/frontend/pages/HomePage.kt
@@ -1,12 +1,34 @@
 package com.knowbuddy.friendsforever.frontend.pages
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import com.knowbuddy.friendsforever.R
 
 class HomePage : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        val firstCard: CardView = findViewById(R.id.firstCard)
+        val secondCard: CardView = findViewById(R.id.secondCard)
+        val thirdCard: CardView = findViewById(R.id.thirdCard)
+
+        // Add onClickListeners to the cards
+        firstCard.setOnClickListener {
+            // Add code to handle the click event
+            val intent = Intent(this, Quiz1::class.java)
+            startActivity(intent)
+        }
+        secondCard.setOnClickListener {
+            // Add code to handle the click event
+            val intent = Intent(this, Quiz2::class.java)
+            startActivity(intent)
+        }
+        thirdCard.setOnClickListener {
+            // Add code to handle the click event
+            val intent = Intent(this, Quiz3::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/knowbuddy/friendsforever/frontend/pages/Quiz2.kt
@@ -0,0 +1,16 @@
+package com.knowbuddy.friendsforever.frontend.pages
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.knowbuddy.friendsforever.R
+
+class Quiz2 : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz2)
+    }
+}
