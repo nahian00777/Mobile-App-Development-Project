3da390ac04a87c0443a11a17cdf7de55950dc488
faysaltaysir <faysaltaysir1435@gmail.com>
2024-12-29 19:22:20 +0600

    previous state saving

--- app/src/main/java/com/knowbuddy/friendsforever/frontend/models/FriendQuestion.kt
+++ app/src/main/java/com/knowbuddy/friendsforever/frontend/models/FriendQuestion.kt
@@ -4,5 +4,6 @@ package com.knowbuddy.friendsforever.frontend.models
 data class Question(
     val question: String,
     val options: List<String>,
-    val correctAnswer: Int // Index of the correct option
+    val correctAnswer: Int, // Index of the correct option
+    var inputtedAnswer: Int
 )
